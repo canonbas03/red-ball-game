ed608c016151b9f8c077141b903a2a266b7e259a
canonbas03 <166402031+canonbas03@users.noreply.github.com>
2024-07-13 21:23:45 +0300

    Added game restart
    When spikes get touched the Level1 scene loads

---
+++ RedBallGame/Assets/Scripts/RestartGame.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    public string playerTag = "Player";
+    public float delay = 1.0f;
+    public string nextScene = "0";
+
+    private bool collisionDetected = false;
+    //private float timer = 0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(playerTag))
+        {
+            //collisionDetected = true;
+            //timer = delay;
+            Invoke("DelayTime", delay);
+        }
+    }
+   
+
+    void DelayTime()
+    {
+
+        SceneManager.LoadScene(nextScene);
+
+    }
+}
